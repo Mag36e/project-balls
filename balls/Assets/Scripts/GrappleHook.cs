8c9fe952486b854350859e515b42f05ab7373847
MagneTree <magnes05@outlook.dk>
2024-02-01 10:12:31 +0100

    Barrwls

--- balls/Assets/Scripts/GrappleHook.cs
+++ balls/Assets/Scripts/GrappleHook.cs
@@ -5,14 +5,15 @@ using UnityEditor;
 using UnityEngine;
 using FishNet;
 using FishNet.Object;
+using UnityEngine.Serialization;
 
 public class GrappleHook : NetworkBehaviour
 {
     public LineRenderer lineRenderer;
     public DistanceJoint2D distanceJoint;
     public List<Transform> hookPoints = new List<Transform>();
-
-    private Vector2 _closestHook;
+    public Vector2 closestHook;
+    public bool hooked = false;
     private void Start()
     {
         foreach(GameObject Hp in GameObject.FindGameObjectsWithTag("Hook")) // hp == hoked Points
@@ -61,6 +62,7 @@ public class GrappleHook : NetworkBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 float closestDistance = float.MaxValue;
+                hooked = true;
                 foreach (Transform target in hookPoints)
                 {
                     float distance = Vector2.Distance(transform.position, target.transform.position);
@@ -68,12 +70,12 @@ public class GrappleHook : NetworkBehaviour
                     if (distance < closestDistance)
                     {
                         closestDistance = distance;
-                        _closestHook = target.transform.position;
+                        closestHook = target.transform.position;
                     }
                 }
                 lineRenderer.SetPosition(0, transform.position);
-                lineRenderer.SetPosition(1, _closestHook);
-                distanceJoint.connectedAnchor = _closestHook;
+                lineRenderer.SetPosition(1, closestHook);
+                distanceJoint.connectedAnchor = closestHook;
                 distanceJoint.enabled = true;
                 lineRenderer.enabled = true;
                 Debug.Log("down");
@@ -81,6 +83,7 @@ public class GrappleHook : NetworkBehaviour
             }
             if(Input.GetKeyUp(KeyCode.Space))
             {
+                hooked = false;
                 distanceJoint.enabled = false;
                 lineRenderer.enabled = false;
                 Debug.Log("up");
---
+++ balls/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float force;
+
+    private Vector2 _closestHook;
+    void Update()
+    {
+        if (gameObject.GetComponent<GrappleHook>().hooked)
+        {
+            _closestHook = gameObject.GetComponent<GrappleHook>().closestHook;
+            if (Input.GetKey(KeyCode.D) && (_closestHook.y / 2 > transform.position.y || _closestHook.x > transform.position.x))
+            {
+                rb.AddForce(transform.right * force);
+                StartCoroutine(Wait(0.05f));
+                Debug.Log("Right");
+            }
+            if (Input.GetKey(KeyCode.A) && (_closestHook.y / 2 > transform.position.y || _closestHook.x < transform.position.x))
+            {
+                rb.AddForce(transform.right * -force);
+                StartCoroutine(Wait(0.05f));
+                Debug.Log("left");
+            }
+        }
+    }
+
+    IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+}
